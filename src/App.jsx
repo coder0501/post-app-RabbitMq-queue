d392a45397eebaf18976061defa1675b336eb807
Abhishek Tiwari <82721930+coder0501@users.noreply.github.com>
2024-08-27 19:29:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import BlogList from './components/BlogList';
-// import BlogDetail from './components/BlogDetail';   
-// import CreateBlog from './components/CreateBlog';
-// import EditBlog from './components/EditBlog';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard'; // Create this component later
 import './App.css';
-// import Task from './components/task';
-// import UpdateTask from './components/UpdateTask';
-// import DeleteTask from './components/DeleteTask';
 
+/**
+ * Main App component for routing and rendering different pages.
+ * @returns {JSX.Element} - The rendered App component with routing.
+ */
 const App = () => {
   return (
     <Router>
@@ -21,14 +16,8 @@ const App = () => {
         <Route path="/" element={
           <div className="home">
             <Signup />
-          </div>    
+          </div>
         } />
-        {/* <Route path="/addTask" element={<Task/>}/>
-        <Route path="/updateTask/:id" element={<UpdateTask/>}/>
-        <Route path="/deleteTask/:id" element={<DeleteTask/>}/>
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-        <Route path="/create" element={<CreateBlog />} />
-        <Route path="/edit/:id" element={<EditBlog />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -38,5 +27,3 @@ const App = () => {
 };
 
 export default App;
-
-
